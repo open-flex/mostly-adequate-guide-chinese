67f4747f81c2b3b2813d6172d6c876fb9aa356f2
xinpingwang <xinping.wang@hotmail.com>
2022-03-26 18:56:03 +0800

    feat: google analytics

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,9 +1,10 @@
-const charpters = Array.from({length: 12}, (_, index)=> `ch${index + 1}`)
+const charpters = Array.from({ length: 12 }, (_, index) => `ch${index + 1}`);
 
 module.exports = {
   title: "JavaScript 函数式编程指南中文版",
   base: "/mostly-adequate-guide-chinese/",
+  plugins: [["@vuepress/google-analytics", { ga: "G-JNFKK7VGLS" }]],
   themeConfig: {
-    sidebar: charpters
-  }
-}
+    sidebar: charpters,
+  },
+};
