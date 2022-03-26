3041286a64910c4564e5c744d6f32f0704abfa12
xinpingwang <xinping.wang@hotmail.com>
2022-03-26 19:32:11 +0800

    refact: google analytics by customer template

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,6 @@ const charpters = Array.from({ length: 12 }, (_, index) => `ch${index + 1}`);
 module.exports = {
   title: "JavaScript 函数式编程指南中文版",
   base: "/mostly-adequate-guide-chinese/",
-  plugins: [["@vuepress/google-analytics", { ga: "G-JNFKK7VGLS" }]],
   themeConfig: {
     sidebar: charpters,
   },
