dc15c6da9b4a03317d9bdfbac93a3779a45999ba
xinpingwang <xinping.wang@hotmail.com>
2022-03-26 18:07:48 +0800

    fix: publish base path

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,7 @@ const charpters = Array.from({length: 12}, (_, index)=> `ch${index + 1}`)
 
 module.exports = {
   title: "JavaScript 函数式编程指南中文版",
+  base: "./",
   themeConfig: {
     sidebar: charpters
   }
